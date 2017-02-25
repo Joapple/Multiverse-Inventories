be340ac3e74158627a98f24d3eb16fc2c3f53d71
Jonathan <Jonathan@EQ-JONATHAN.equimed.ca>
2017-02-25 15:57:11 -0500

    Begin of fixing errors

--- src/com/onarandombox/multiverseinventories/DefaultGroupingConflict.java
+++ src/com/onarandombox/multiverseinventories/DefaultGroupingConflict.java
@@ -7,72 +7,59 @@ import com.onarandombox.multiverseinventories.api.share.Shares;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple implementation of GroupingConflict.
- */
+/** A simple implementation of GroupingConflict. */
 final class DefaultGroupingConflict implements GroupingConflict {
-
-    private WorldGroupProfile groupOne;
-    private WorldGroupProfile groupTwo;
-    private Shares conflictingShares;
-
-    public DefaultGroupingConflict(WorldGroupProfile groupOne, WorldGroupProfile groupTwo, Shares conflictingShares) {
-        this.groupOne = groupOne;
-        this.groupTwo = groupTwo;
-        this.conflictingShares = conflictingShares;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public WorldGroupProfile getFirstGroup() {
-        return this.groupOne;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public WorldGroupProfile getSecondGroup() {
-        return this.groupTwo;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Shares getConflictingShares() {
-        return this.conflictingShares;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<String> getConflictingWorlds() {
-        List<String> worlds = new ArrayList<String>();
-        for (String world : this.getFirstGroup().getWorlds()) {
-            if (this.getSecondGroup().getWorlds().contains(world)) {
-                worlds.add(world);
-            }
-        }
-        return worlds;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getWorldsString() {
-        StringBuilder builder = new StringBuilder();
-        for (String world : this.getConflictingWorlds()) {
-            if (!builder.toString().isEmpty()) {
-                builder.append(", ");
-            }
-            builder.append(world);
-        }
-        return builder.toString();
-    }
+	
+	private final WorldGroupProfile groupOne;
+	private final WorldGroupProfile groupTwo;
+	private final Shares conflictingShares;
+	
+	public DefaultGroupingConflict(WorldGroupProfile groupOne, WorldGroupProfile groupTwo, Shares conflictingShares) {
+		this.groupOne = groupOne;
+		this.groupTwo = groupTwo;
+		this.conflictingShares = conflictingShares;
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public WorldGroupProfile getFirstGroup() {
+		return this.groupOne;
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public WorldGroupProfile getSecondGroup() {
+		return this.groupTwo;
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public Shares getConflictingShares() {
+		return this.conflictingShares;
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public List<String> getConflictingWorlds() {
+		List<String> worlds = new ArrayList<>();
+		for(String world : this.getFirstGroup().getWorlds()) {
+			if(this.getSecondGroup().getWorlds().contains(world)) {
+				worlds.add(world);
+			}
+		}
+		return worlds;
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public String getWorldsString() {
+		StringBuilder builder = new StringBuilder();
+		for(String world : this.getConflictingWorlds()) {
+			if(!builder.toString().isEmpty()) {
+				builder.append(", ");
+			}
+			builder.append(world);
+		}
+		return builder.toString();
+	}
 }
-
--- src/com/onarandombox/multiverseinventories/WeakWorldProfileManager.java
+++ src/com/onarandombox/multiverseinventories/WeakWorldProfileManager.java
@@ -7,41 +7,34 @@ import com.onarandombox.multiverseinventories.api.profile.WorldProfileManager;
 import java.util.Map;
 import java.util.WeakHashMap;
 
-/**
- * Simple implementation of WorldProfileManager.
- */
+/** Simple implementation of WorldProfileManager. */
 final class WeakWorldProfileManager implements WorldProfileManager {
-
-    private Map<String, WorldProfile> worldProfiles = new WeakHashMap<String, WorldProfile>();
-    private Inventories inventories;
-
-    public WeakWorldProfileManager(Inventories inventories) {
-        this.inventories = inventories;
-    }
-
-    private Inventories getInventories() {
-        return this.inventories;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void addWorldProfile(WorldProfile worldProfile) {
-        this.worldProfiles.put(worldProfile.getWorld().toLowerCase(), worldProfile);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public WorldProfile getWorldProfile(String worldName) {
-        WorldProfile worldProfile = this.worldProfiles.get(worldName.toLowerCase());
-        if (worldProfile == null) {
-            worldProfile = new DefaultWorldProfile(this.getInventories(), worldName);
-            this.addWorldProfile(worldProfile);
-        }
-        return worldProfile;
-    }
+	
+	private final Map<String, WorldProfile> worldProfiles = new WeakHashMap<>();
+	private final Inventories inventories;
+	
+	public WeakWorldProfileManager(Inventories inventories) {
+		this.inventories = inventories;
+	}
+	
+	private Inventories getInventories() {
+		return this.inventories;
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public void addWorldProfile(WorldProfile worldProfile) {
+		this.worldProfiles.put(worldProfile.getWorld().toLowerCase(), worldProfile);
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public WorldProfile getWorldProfile(String worldName) {
+		WorldProfile worldProfile = this.worldProfiles.get(worldName.toLowerCase());
+		if(worldProfile == null) {
+			worldProfile = new DefaultWorldProfile(this.getInventories(), worldName);
+			this.addWorldProfile(worldProfile);
+		}
+		return worldProfile;
+	}
 }
-
